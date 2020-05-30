0c26cf918347acdc2c8945d7a10964f4edc443dc
soni-ratnesh <ratneshk.99@gmail.com>
2020-05-30 20:26:07 +0530

    Add create app

---
+++ application/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+
+def create_app():
+    """Initialize the core application."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+
+    with app.app_context():
+        return app
