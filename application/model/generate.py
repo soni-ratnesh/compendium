f7ac5b4fcf5a2a44996470272366b57846a0fb97
soni-ratnesh <ratneshk.99@gmail.com>
2020-06-06 00:29:00 +0530

    Add predict
    Added Predict function to generate summary of text.

--- application/model/generate.py
+++ application/model/generate.py
@@ -1,5 +1,6 @@
 import dill
 import torch
+import spacy
 from application.model.model import Encoder, Decoder, Attention, Seq2Seq
 
 # setup device to cpu
@@ -29,3 +30,38 @@ model = Seq2Seq(enc, dec, ARTICLE_PAD_IDX, device)
 
 model.load_state_dict(torch.load('./data/model.brain', map_location=device))
 
+
+def predict(sentence, src_field, trg_field, model, max_len=50):
+    model.eval()
+    nlp = spacy.load('en')
+    tokens = [token.text.lower() for token in nlp(sentence)]
+    tokens = [src_field.init_token] + tokens + [src_field.eos_token]
+    src_indexes = [src_field.vocab.stoi[token] for token in tokens]
+
+    src_tensor = torch.LongTensor(src_indexes).unsqueeze(1)
+    src_len = torch.LongTensor([len(src_indexes)])
+
+    with torch.no_grad():
+        encoder_outputs, hidden = model.encoder(src_tensor, src_len)
+
+    mask = model.create_mask(src_tensor)
+    trg_indexes = [trg_field.vocab.stoi[trg_field.init_token]]
+    attentions = torch.zeros(max_len, 1, len(src_indexes))
+
+    for i in range(max_len):
+
+        trg_tensor = torch.LongTensor([trg_indexes[-1]])
+
+        with torch.no_grad():
+            output, hidden, attention = model.decoder(trg_tensor, hidden, encoder_outputs, mask)
+
+        attentions[i] = attention
+        pred_token = output.argmax(1).item()
+        trg_indexes.append(pred_token)
+
+        if pred_token == trg_field.vocab.stoi[trg_field.eos_token]:
+            break
+
+    trg_tokens = [trg_field.vocab.itos[i] for i in trg_indexes]
+
+    return trg_tokens[1:], attentions[:len(trg_tokens) - 1]
