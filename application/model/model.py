8258117f7a0cfbef8f826a147fdc057194072b53
soni-ratnesh <ratneshk.99@gmail.com>
2020-06-05 22:15:12 +0530

    model
    Added Attention class.

--- application/model/model.py
+++ application/model/model.py
@@ -6,13 +6,14 @@ import torch.nn.functional as F
 import math
 import spacy
 
+
 class Encoder(nn.Module):
 
-    def __init__(self, vocab, embeding_dim, encoder_hidden_dim, decoder_hidden_dim, dropout):
+    def __init__(self, vocab, embedding_dim, encoder_hidden_dim, decoder_hidden_dim, dropout):
         super().__init__()
 
-        self.embedding = nn.Embedding(vocab, embeding_dim)
-        self.rnn = nn.GRU(embeding_dim, encoder_hidden_dim, bidirectional=True)
+        self.embedding = nn.Embedding(vocab, embedding_dim)
+        self.rnn = nn.GRU(embedding_dim, encoder_hidden_dim, bidirectional=True)
         self.fc = nn.Linear(encoder_hidden_dim * 2, decoder_hidden_dim)
 
         self.dropout = nn.Dropout(p=dropout)
@@ -25,3 +26,25 @@ class Encoder(nn.Module):
         hidden = torch.tanh(self.fc(torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1)))
 
         return outputs, hidden
+
+
+class Attention(nn.Module):
+
+    def __init__(self, enc_hid_dim, dec_hid_dim):
+        super().__init__()
+        self.attn = nn.Linear((enc_hid_dim * 2) + dec_hid_dim, dec_hid_dim)
+        self.v = nn.Linear(dec_hid_dim, 1, bias=False)
+
+    def forward(self, hidden, encoder_outputs, mask):
+        batch_size = encoder_outputs.shape[1]
+        src_len = encoder_outputs.shape[0]
+
+        hidden = hidden.unsqueeze(1).repeat(1, src_len, 1)
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+
+        energy = torch.tanh(self.attn(torch.cat((hidden, encoder_outputs), dim=2)))
+
+        attention = self.v(energy).squeeze(2)
+        attention = attention.masked_fill(mask == 0, -1e10)
+
+        return F.softmax(attention, dim=1)
